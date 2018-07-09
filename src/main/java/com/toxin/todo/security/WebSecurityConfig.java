68a66f42ca664cad29a33c27bf95968f6a758f37
toxin <yuri.belousov.1998@gmail.com>
2018-07-09 19:00:38 +0300

    disavle base aouth

--- src/main/java/com/toxin/todo/security/WebSecurityConfig.java
+++ src/main/java/com/toxin/todo/security/WebSecurityConfig.java
@@ -35,7 +35,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http
+	    .csrf().disable()
+	    .httpBasic().disable();
     }
 
     @Override
